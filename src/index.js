acbf58bd7089022d1ee100952671c3301721f463
xucaihong <xucaihongleying365.com>
2018-11-24 17:07:01 +0800

    引入jquery

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import _ from 'lodash';
+import j from 'jquery'
 function component() {
-    let element = document.createElement('div');
+    var element = j('<div></div>');
+
   
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
+    element.html(_.join(['Hello','webpack'], ' '))
+
+     return element.get(0);
   }
   
   document.body.appendChild(component());
