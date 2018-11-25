dbbb04e901063943b28cfcce99d8b66345c374c9
xucaihong <xucaihongleying365.com>
2018-11-25 10:18:13 +0800

    adjust

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import _ from 'lodash';
-import j from 'jquery'
+import j from 'jquery';
+import foo from './foo'
+import printMe from './print.js';
+
 function component() {
     var element = j('<div></div>');
-
+    
   
     // Lodash, currently included via a script, is required for this line to work
-    element.html(_.join(['Hello','webpack'], ' '))
+    element.html(_.join(['Hii','webpack'], ' '))
 
      return element.get(0);
   }
   
   document.body.appendChild(component());
+
+  console.log(foo)
+  console.log(foo())
